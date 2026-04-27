69f01974831c873cdf46aee36ebe1c2c15dfacf8
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-04-27 18:30:46 +0300

    Initial project commit

---
+++ Offices/Program.cs
@@ -0,0 +1,28 @@
+
+namespace Offices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            // app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
